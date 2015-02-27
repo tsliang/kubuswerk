f024448595451f74ea61911d2b2887828328340c
Tai Sassen-Liang <tai.sassen-liang@ef.com>
2015-02-27 14:41:01 -0500

    Add arduino code

---
+++ arduino/kubuswerk/kubuswerk.ino
@@ -0,0 +1,19 @@
+
+int RED_PIN = 5;
+int GRN_PIN = 6;
+
+int mode = 0;
+void setup() {
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(GRN_PIN, OUTPUT);
+  Serial.begin(57600);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    parseOpCode(Serial.read());
+  }
+}
+
+void parseOpCode(int opCode) {
+}
